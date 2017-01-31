903fbb00bb7213ccafc4da0c2f928f3424c11540
TKatra <tobiaskatra@hotmail.com>
2017-01-31 01:39:42 +0100

    changed devices in sites to arrays to make them more easy to use

--- app/modules/sites/controllers/siteController.js
+++ app/modules/sites/controllers/siteController.js
@@ -7,7 +7,14 @@ exports.get = function(req, res) {
   var result = [];
 
   for (var key in sites) {
+    var deviceArray = [];
     if (sites.hasOwnProperty(key)) {
+
+      for (var device in sites[key].devices) {
+        deviceArray.push(sites[key].devices[device]);
+      }
+      sites[key].devices = deviceArray;
+
       result.push(sites[key]);
     }
   }
